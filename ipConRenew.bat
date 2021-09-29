5046fc4a3d98a9c525456a7eedbc8a83b606cb8e
Byron <53088136+byronbytes@users.noreply.github.com>
2021-09-29 08:29:43 -0400

    Update ipConRenew.bat

--- ipConRenew.bat
+++ ipConRenew.bat
@@ -3,3 +3,7 @@ ipconfig /release
 echo The internet may be temporarily disabled, please wait.
 @echo on
 ipconfig /renew
+@echo off
+echo ipCOnFix has completed, you may close this script.
+@echo on
+pause
