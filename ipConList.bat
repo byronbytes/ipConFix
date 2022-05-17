267e7ad1d8864d35f24b7cce2703d15f426b61a1
Matty <53088136+byronbytes@users.noreply.github.com>
2022-05-17 12:40:36 -0400

    Update ipConList.bat

--- ipConList.bat
+++ ipConList.bat
@@ -1,7 +1,10 @@
+:: Lists through ALL ipconfig commands, everything informational you may need is listed here.
 ipconfig /all
 ipconfig /displaydns
 ipconfig /showclassid
+
+:: TODO: Fix this? Maybe put it into ipConFix as a sub-setting.
 @echo off
-echo ipConFix has ran through all commands.
+echo You may close ipConFix now.
 @echo on
 pause
