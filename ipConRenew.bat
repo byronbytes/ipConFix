07fd0056ee047ba44df08aed839f268aa9b0cd9e
Matty <53088136+byronbytes@users.noreply.github.com>
2022-05-17 12:24:13 -0400

    Delete ipConRenew.bat

--- ipConRenew.bat
+++
@@ -1,9 +0,0 @@
-ipconfig /release
-@echo off
-echo The internet may be temporarily disabled, please wait.
-@echo on
-ipconfig /renew
-@echo off
-echo ipConFix has completed, you may close this script.
-@echo on
-pause
