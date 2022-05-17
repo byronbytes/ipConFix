872496c4f5717deb757c7cbea3ecf5f52b194611
Matty <53088136+byronbytes@users.noreply.github.com>
2022-05-17 12:23:57 -0400

    Update ipConFix.bat

--- ipConFix.bat
+++ ipConFix.bat
@@ -1,8 +1,18 @@
+color 02
+
+@echo off
+echo ipConFix will temporarily disable internet connectivity, please close anything that may use internet before running this.
+@echo on
+pause
+
+ipconfig /release
+ipconfig /release6
+
 ipconfig /renew
 ipconfig /renew6
 ipconfig /flushdns
 
 @echo off
-echo ipConFix has ran through all commands, you may close the application now.
+echo ipConFix has ran through all commands neccesary, you may close the application now.
 @echo on
 pause
